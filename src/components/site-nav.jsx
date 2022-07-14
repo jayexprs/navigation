467d457b6ada6b40ce5ad96095242b5b4861eef5
Jay McCormick <jayexprs@gmail.com>
2022-07-14 12:32:11 -0400

    comments on the site-nav component

--- src/components/site-nav.jsx
+++ src/components/site-nav.jsx
@@ -1,7 +1,13 @@
 export default function SiteNav(props){
+  // these functions get the data that comes in as a prop, in layouts/SiteLayout.astro file
+  // you write functions to keep the data 'reactive', meaning you can update the data file and the component
+  // updates with the latest data
   const where = () => props.where.toString().split('3000').pop();
   const menu = () => props.menu;
 
+  // <For ></For> is a built-in SolidJS component that loops over the data you pass in
+  // the {(item) => ...} is the function that tells Solid what to do with each item in the data
+  // here it's giving back an <a>anchor</a> tag, with a class to indicate which <a> is the selected one
   return (
     <nav class="nav">
       <For each={menu()}>{

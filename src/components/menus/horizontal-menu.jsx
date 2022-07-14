eae055a0b7aefb17437eebb2f3e15cb5010ea4f4
Jay McCormick <jayexprs@gmail.com>
2022-07-14 10:22:39 -0400

    horiztonal menu component POC

--- src/components/menus/horizontal-menu.jsx
+++ src/components/menus/horizontal-menu.jsx
@@ -1,8 +1,12 @@
+import VerticalMenu from './vertical-menu.jsx';
+
 export default function HorizontalMenu(props){
   const tree = () => props.tree;
   return (
     <For each={tree()}>{(branch) =>
-      <p id={branch}>HI JEANA</p>
+      <div class="box" id={branch}>{branch.name}
+        <VerticalMenu tree={branch.links} />
+      </div>
     }
     </For>
   )

212f6b10e75f8e990a2c000ee054b262a0fd892f
Jay McCormick <jayexprs@gmail.com>
2022-09-09 10:09:23 -0400

    added file back

---
+++ src/components/site.js
@@ -0,0 +1,6 @@
+export default SITE = [
+  {space: 'Overview', links: ['/']},
+  {space: 'Vertical Menu', links: ['/vertical-menu']},
+  {space: 'Horizontal Menu', links: ['/horizontal-menu']},
+  {space: 'README', links: ['/readme']}
+]

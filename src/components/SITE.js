2c65ab6c8785a7a4063e8abb5074bad53804ef2d
Jay McCormick <jayexprs@gmail.com>
2022-09-09 10:07:57 -0400

    trying to delete file to resolve netlify issue

--- src/components/SITE.js
+++
@@ -1,6 +0,0 @@
-export default SITE = [
-  {space: 'Overview', links: ['/']},
-  {space: 'Vertical Menu', links: ['/vertical-menu']},
-  {space: 'Horizontal Menu', links: ['/horizontal-menu']},
-  {space: 'README', links: ['/readme']}
-]
